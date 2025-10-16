a20edb587bccf5a59d88e585c684204d0fa0f82b
lorenzomattei <matteilorenzo@outlook.it>
2025-10-16 22:29:31 +0200

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Istituti Redentore",
+    pageTitle: "Giardino Digitale",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
